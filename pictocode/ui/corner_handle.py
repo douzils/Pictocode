6f223cfb28b68d5dd8d6477dda3421af1f938695
Tom Bigot <32032507+douzils@users.noreply.github.com>
2025-06-24 10:57:05 +0200

    Improve dock split preview

---
+++ pictocode/ui/corner_handle.py
@@ -0,0 +1,23 @@
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPainter, QPen
+
+
+class CornerHandle(QWidget):
+    """Small handle shown in the bottom right corner of dock widgets."""
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setObjectName("corner_handle")
+        self.setFixedSize(12, 12)
+        self.setCursor(Qt.ArrowCursor)
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+        pen = QPen(self.palette().color(self.foregroundRole()), 1)
+        painter.setPen(pen)
+        for i in range(3):
+            offset = 3 + i * 3
+            painter.drawLine(0, self.height() - offset, self.width() - offset, self.height())
+        painter.end()
